b3f93db7fde2b121e0810fdbf9b3c0a0bf8ff7c2
zhuoyangjiang <70708249+zhuoyangjiang@users.noreply.github.com>
2020-09-17 21:40:35 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+// Author: zhuoyangjiang zfj5038@psu.edu
+
+#include "stdio.h"
+#include "stdlib.h"
+#include <readline/readline.h>
+#include <readline/history.h>
+
+
+int main()
+{
+    char *str;
+    str = readline("Enter your name: ");
+    printf("Hello,%s!\n", str);
+    free(str);
+    return 0;
+}
